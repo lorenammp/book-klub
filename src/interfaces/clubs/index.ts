af25fa6d160af3e95de6f76094d2a6e4115ae4d0
Carlos Henrique <henriquerodriguez0@gmail.com>
2022-09-06 14:30:24 -0300

    feat created interfaces

---
+++ src/interfaces/clubs/index.ts
@@ -0,0 +1,13 @@
+export interface IClubRequest {
+    name: string,
+    description: string,
+    adm_id: string
+}
+
+export interface IClub{
+    adm_id: string,
+    name: string,
+    description: string,
+    isActive: boolean,
+    created_at: Date,
+}
