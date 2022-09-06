281aae9ec1c998bdddbdd76baff96f2a5f26f5c6
Jamily Lima <jamilylima70@gmail.com>
2022-09-06 16:39:40 -0300

    feat:create middlewares Auth/Adm

--- src/middlewares/adm.middleware.ts
+++ src/middlewares/adm.middleware.ts
@@ -3,7 +3,7 @@ import { AppError } from '../errors/appError';
 
 const admMiddlleware = async (req: Request, res: Response, next: NextFunction) => {
     
-  const adm = req.body.isAdm
+  const adm = req.body.isAdm;
   
   if (adm === false) {
     throw new AppError(403,"Not authorization")
