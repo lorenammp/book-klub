0b43e82e4792356b880f321f5bb9b16d1ccb56d3
Leonardo <yleeoembd@gmail.com>
2022-09-06 15:18:03 -0300

    feat: criando entidades

--- src/entities/clubs.entity.ts
+++ src/entities/clubs.entity.ts
@@ -1,19 +1,32 @@
-import { OneToOne, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+  OneToOne,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from "typeorm";
 import { v4 as uuid } from "uuid";
+import { UsersEntity } from "./users.entity";
 
 @Entity("clubs")
 export class ClubsEntity {
   @PrimaryGeneratedColumn()
   readonly Club_id: string;
 
+  @OneToOne(() => UsersEntity, (UsersEntity) => UsersEntity.id)
+  adm_id: string;
+
   @Column({ length: 50 })
   name: string;
 
-  @Column({ length: 50 })
-  author: string;
+  @Column({ length: 300 })
+  description: string;
 
-  @OneToOne(() => CategoriesEntity, (CategoriesEntity) => CategoriesEntity.id)
-  category_id: CategoriesEntity;
+  @Column()
+  isActive: boolean;
+
+  @CreateDateColumn()
+  created_At: Date;
 
   constructor() {
     if (!this.Club_id) {
