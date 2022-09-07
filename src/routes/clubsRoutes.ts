1c097c16ddbd905ae5d2e232647996afef2637a8
Clara Cavalcantee <claracarvc@gmail.com>
2022-09-07 11:37:56 -0300

    feat: cração da rota de get de clubs

--- src/routes/clubsRoutes.ts
+++ src/routes/clubsRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import clubsListController from "../controllers/clubs/clubsList.controller";
 import createClubController from "../controllers/clubs/createClub.controller";
 import AuthMiddlewares from "../middlewares/auth.middleware";
 
 const clubRouter = Router();
 
 clubRouter.post("", AuthMiddlewares, createClubController);
+clubRouter.get("", clubsListController)
 
 export default clubRouter;
