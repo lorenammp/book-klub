9c548d883a789bb18cba808dc6ff92570b595f4a
Jamily Lima <jamilylima70@gmail.com>
2022-09-08 14:55:41 -0300

    Criação da rota delete/club

--- src/routes/clubsRoutes.ts
+++ src/routes/clubsRoutes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 
 import clubsListController from "../controllers/clubs/clubsList.controller";
 import createClubController from "../controllers/clubs/createClub.controller";
+import deleteClubController from "../controllers/clubs/deleteClub.controller";
+import admMiddlleware from "../middlewares/adm.middleware";
 import AuthMiddlewares from "../middlewares/auth.middleware";
 
 const clubRouter = Router();
 
 clubRouter.post("", AuthMiddlewares, createClubController);
 clubRouter.get("", clubsListController)
+clubRouter.delete("/:id",AuthMiddlewares,admMiddlleware,deleteClubController)
 
 export default clubRouter;
