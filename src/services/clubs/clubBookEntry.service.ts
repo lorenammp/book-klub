9800f1161f4849f3e20d855ad896b807071c51b7
lorenammp <lorenammpaula@gmail.com>
2022-09-11 14:23:14 -0300

    adicionado eager true à entidade user_club

--- src/services/clubs/clubBookEntry.service.ts
+++ src/services/clubs/clubBookEntry.service.ts
@@ -3,32 +3,31 @@ import { BooksEntity } from "../../entities/books.entity";
 import { ClubsEntity } from "../../entities/clubs.entity";
 import { ClubBookEntity } from "../../entities/club_book.entity";
 import { AppError } from "../../errors/appError";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
-const clubBookEntryService = async(clubId: string, bookId: string)=>{
-    const bookRepository = AppDataSource.getRepository(BooksEntity);
-    const clubRepository = AppDataSource.getRepository(ClubsEntity);
-    const clubBookRepository = AppDataSource.getRepository(ClubBookEntity);
+const clubBookEntryService = async (clubId: string, bookId: string) => {
+  const bookRepository = AppDataSource.getRepository(BooksEntity);
+  const clubRepository = AppDataSource.getRepository(ClubsEntity);
+  const clubBookRepository = AppDataSource.getRepository(ClubBookEntity);
 
-    const bookAlready = await bookRepository.findBy({id: bookId});
-    const clubAlready = await clubRepository.findBy({id: clubId})
+  const bookAlready = await bookRepository.findBy({ id: bookId });
+  const clubAlready = await clubRepository.findBy({ id: clubId });
 
-    if(!bookAlready){
-        new AppError(400, "Book not found")
-    }
-    if(!clubAlready){
-        new AppError(400, "Club not found")
-    }
+  if (!bookAlready) {
+    new AppError(400, "Book not found");
+  }
+  if (!clubAlready) {
+    new AppError(400, "Club not found");
+  }
 
-    const newClubBook = clubBookRepository.create({
-        id: uuidv4(),
-        book: bookId,
-        club: clubId
-    })
+  const newClubBook = clubBookRepository.create({
+    book: bookId,
+    club: clubId,
+  });
 
-    await clubBookRepository.save(newClubBook)
+  await clubBookRepository.save(newClubBook);
 
-    return "Book added successfully"
-}
+  return "Book added successfully";
+};
 
 export default clubBookEntryService;
