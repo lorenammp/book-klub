321fe07d807574bc6e8723f85dbe67dfdfbdb59d
Gabriela-LC <gabrielacavalcanti6@gmail.com>
2022-09-13 09:05:06 -0300

    fix: correção da rota de adicionar livro ao clube para retornar erro quando id do livro ou do clube não existem

--- src/services/clubs/clubBookEntry.service.ts
+++ src/services/clubs/clubBookEntry.service.ts
@@ -3,21 +3,20 @@ import { BooksEntity } from "../../entities/books.entity";
 import { ClubsEntity } from "../../entities/clubs.entity";
 import { ClubBookEntity } from "../../entities/club_book.entity";
 import { AppError } from "../../errors/appError";
-import { v4 as uuidv4 } from "uuid";
 
 const clubBookEntryService = async (clubId: string, bookId: string) => {
   const bookRepository = AppDataSource.getRepository(BooksEntity);
   const clubRepository = AppDataSource.getRepository(ClubsEntity);
   const clubBookRepository = AppDataSource.getRepository(ClubBookEntity);
 
-  const bookAlready = await bookRepository.findBy({ id: bookId });
-  const clubAlready = await clubRepository.findBy({ id: clubId });
+  const bookAlready = await bookRepository.findOneBy({ id: bookId });
+  const clubAlready = await clubRepository.findOneBy({ id: clubId });
 
   if (!bookAlready) {
-    new AppError(400, "Book not found");
+    throw new AppError(404, "Book not found");
   }
   if (!clubAlready) {
-    new AppError(400, "Club not found");
+    throw new AppError(404, "Club not found");
   }
 
   const newClubBook = clubBookRepository.create({
