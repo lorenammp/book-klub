d1a4830a7788269969e2a70730857c1ea9e5a869
lorenammp <lorenammpaula@gmail.com>
2022-09-14 00:25:25 -0300

    feat: desenvolvido endpoint para cadastro de administrador da aplicação, além de fix nos testes de books

--- src/__test__/bookTests/booksTests.spec.ts
+++ src/__test__/bookTests/booksTests.spec.ts
@@ -2,191 +2,164 @@ import request from "supertest";
 import { DataSource } from "typeorm";
 import app from "../../app";
 import AppDataSource from "../../data-source";
-import { mockedBookRegister, mockedCategoryRegister, mockedSecondBookRegister, mockedUserLogin, mockedUserRegister } from "../mocks";
+import {
+  fakeId,
+  mockedAdmUserLogin,
+  mockedAdmUserRegister,
+  mockedBookRegister,
+  mockedCategoryRegister,
+  mockedSecondBookRegister,
+  mockedUserLogin,
+  mockedUserRegister,
+  mockedWrongBook,
+} from "../mocks";
 
-describe("Testing books routes", ()=>{
-    let connection: DataSource;
-    let bookId: string;
-    
-    beforeAll(async ()=>{
-        await AppDataSource.initialize()
-            .then((res)=>{
-                connection = res;
-            })
-            .catch((err) =>{
-                console.log("Error during Data Source initialization", err)
-            });
-        
-        await request(app).post("/users").send(mockedUserRegister);
-    })
+describe("Testing books routes", () => {
+  let connection: DataSource;
+  let bookId: string;
 
-    afterAll(async () => {
-        await connection.destroy();
-    });
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => {
+        connection = res;
+      })
+      .catch((err) => {
+        console.log("Error during Data Source initialization", err);
+      });
 
-    test("POST /books - Must be able to create a book", async ()=>{
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
+    await request(app).post("/users").send(mockedUserRegister);
+    await request(app).post("/users/adm").send(mockedAdmUserRegister);
+  });
 
-        const createCategory = await request(app)
-        .post("/categories")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedCategoryRegister)
+  afterAll(async () => {
+    await connection.destroy();
+  });
 
-        mockedBookRegister.categoryId = createCategory.body.id
+  test("POST /books - Must be able to create a book", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedUserLogin);
 
-        const response = await request(app)
-        .post("/books")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedBookRegister);
+    const createCategory = await request(app)
+      .post("/categories")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedCategoryRegister);
 
-        expect(response.body).toHaveProperty("id");
-        expect(response.body).toHaveProperty("name");
-        expect(response.body).toHaveProperty("author");
-        expect(response.body).toHaveProperty("category");
-        expect(response.status).toBe(201);
-    })
+    mockedBookRegister.categoryId = createCategory.body.id;
 
-    test("POST /books - Shold not be able to create a book with a repeated name and author", async ()=>{
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
+    const response = await request(app)
+      .post("/books")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedBookRegister);
 
-        const createCategory = await request(app)
-        .post("/categories")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedCategoryRegister)
+    bookId = response.body.id;
 
-        mockedBookRegister.categoryId = createCategory.body.id
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("author");
+    expect(response.body).toHaveProperty("category");
+    expect(response.status).toBe(201);
+  });
 
-        const response = await request(app)
-        .post("/books")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedBookRegister);
+  test("POST /books - Shold not be able to create a book with a repeated name and author", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedUserLogin);
 
-        expect(response.body).toHaveProperty("message");
-        expect(response.status).toBe(400);
-    })
+    const response = await request(app)
+      .post("/books")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedBookRegister);
 
-    test("POST /books - Shold not be able to create a book without authentication", async()=>{
-        const createCategory = await request(app)
-        .post("/categories")
-        .send(mockedCategoryRegister)
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(400);
+  });
 
-        mockedBookRegister.categoryId = createCategory.body.id
-        
-        const response = await request(app)
-        .post("/books")
-        .send(mockedBookRegister);
+  test("POST /books - Shold not be able to create a book without authentication", async () => {
+    const response = await request(app).post("/books").send(mockedBookRegister);
 
-        expect(response.body).toHaveProperty("message");
-        expect(response.status).toBe(401);
-    })
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
 
-    test("POST /books - Shold not be able to create a book with missing properties", async ()=>{
-        const createCategory = await request(app)
-        .post("/categories")
-        .send(mockedCategoryRegister)
+  test("POST /books - Shold not be able to create a book with missing properties", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedUserLogin);
 
-        mockedBookRegister.categoryId = createCategory.body.id
-        mockedBookRegister.name = ""
-        mockedBookRegister.author = ""
+    const response = await request(app)
+      .post("/books")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedWrongBook);
 
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(400);
+  });
 
-        const response = await request(app)
-        .post("/books")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedBookRegister);
+  test("POST /books - Shold not be able to create a book with a non-existent category", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedUserLogin);
 
-        expect(response.body).toHaveProperty("message");
-        expect(response.status).toBe(400);
-    })
+    const response = await request(app)
+      .post("/books")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedBookRegister);
 
-    test("POST /books - Shold not be able to create a book with a non-existent category", async ()=>{
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(400);
+  });
 
-        const response = await request(app)
-        .post("/books")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedBookRegister);
+  test("GET /books - Should be able to list all books", async () => {
+    const response = await request(app).get("/books");
 
-        expect(response.body).toHaveProperty("message");
-        expect(response.status).toBe(400);
-    })
-    
-    test("GET /books - Should be able to list all books", async ()=>{
-        const response = await request(app)
-        .get("/books")
-        
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("map");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("map");
+  });
 
-    })
+  test("DELETE /books/:id - Shold be able to delete a book", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedAdmUserLogin);
 
-    test("DELETE /books/:id - Shold be able to delete a book", async()=>{
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
+    const response = await request(app)
+      .delete(`/books/${bookId}`)
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`);
 
-        const createCategory = await request(app)
-        .post("/categories")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedCategoryRegister)
+    expect(response.status).toBe(204);
+  });
 
-        mockedBookRegister.categoryId = createCategory.body.id
+  test("DELETE /books/:id - Shold not be able a book without authorization", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedUserLogin);
 
-        const createBook = await request(app)
-        .post("/books")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedBookRegister);
+    const createCategory = await request(app)
+      .post("/categories")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedCategoryRegister);
 
-        const books = await request(app).get("/books")
+    mockedBookRegister.categoryId = createCategory.body.id;
 
-        const response = await request(app)
-        .delete(`/books/${books.body[0].id}`)
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        
-        expect(response.status).toBe(204)
-    });
+    const createBook = await request(app)
+      .post("/books")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedBookRegister);
 
-    test("DELETE /books/:id - Shold not be able a book without authorization", async ()=>{
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
+    const response = await request(app).delete(`/books/${createBook.body.id}`);
 
-        const createCategory = await request(app)
-        .post("/categories")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedCategoryRegister)
+    expect(response.status).toBe(401);
+  });
 
-        mockedBookRegister.categoryId = createCategory.body.id
+  test("DELETE /books/:id - Shold not be able a book that doesn't exist", async () => {
+    const userLoginResponse = await request(app)
+      .post("/users/login")
+      .send(mockedUserLogin);
 
-        const createBook = await request(app)
-        .post("/books")
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        .send(mockedBookRegister);
+    const response = await request(app)
+      .delete(`/books/66047662-dfa2-4f45-a557-88bc2b9ed294`)
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`);
 
-        const response = await request(app)
-        .delete(`/books/${createBook.body.id}`)
-        
-        expect(response.status).toBe(401)
-    })
-
-    test("DELETE /books/:id - Shold not be able a book that doesn't exist", async ()=>{
-        const userLoginResponse = await request(app)
-        .post("/users/login")
-        .send(mockedUserLogin);
-
-        const response = await request(app)
-        .delete(`/books/66047662-dfa2-4f45-a557-88bc2b9ed294`)
-        .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-        
-        expect(response.status).toBe(400)
-    })
-})
+    expect(response.status).toBe(401);
+  });
+});
