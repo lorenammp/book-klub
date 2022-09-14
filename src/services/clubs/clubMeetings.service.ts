101af8bf78720b7ee1fe7b730191e4338391cf6b
lorenammp <lorenammpaula@gmail.com>
2022-09-14 08:50:49 -0300

    feat: desenvolvida funcionalidade que permite enviar e-mails para os membros de um clube ao criar uma nova reunião

--- src/services/clubs/clubMeetings.service.ts
+++ src/services/clubs/clubMeetings.service.ts
@@ -2,9 +2,10 @@ import AppDataSource from "../../data-source";
 import { MeetingsEntity } from "../../entities/meetings.entity";
 import { ClubsEntity } from "../../entities/clubs.entity";
 import { AppError } from "../../errors/appError";
-import SendEmailController from "../../controllers/email/sendEmail";
 import { UsersClubsEntity } from "../../entities/user_club.entity";
 import { UsersEntity } from "../../entities/users.entity";
+import { sendEmail } from "../../utils";
+import { emailMessage } from "../../utils/email.message";
 
 const clubMeetingsService = async (
   clubId: string,
@@ -47,10 +48,10 @@ const clubMeetingsService = async (
   clubUsers.forEach((userEmail) => users.push(userEmail.users_email));
 
   const subject = emailSubject;
-  const text = "<h1>testando envio de e-mails</h1>";
+  const text = emailMessage(clubIdd.name, date, hour, description);
   const to = users.join();
 
-  await SendEmailController({ subject, text, to });
+  await sendEmail({ subject, text, to });
 
   return newMeeting;
 };
