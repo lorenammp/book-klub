ce3078883a8d6c3aef4c9218bf02d12a85c2fdcd
lorenammp <lorenammpaula@gmail.com>
2022-09-14 11:04:02 -0300

    fix: alterações verificadas em reunião nos arquivos .env e na lista de clubes

--- src/controllers/clubs/clubBooksEntry.controller.ts
+++ src/controllers/clubs/clubBooksEntry.controller.ts
@@ -2,19 +2,17 @@ import { Request, Response } from "express";
 import { handleError } from "../../errors/appError";
 import clubBookEntryService from "../../services/clubs/clubBookEntry.service";
 
-const clubBookEntryController = async(req: Request, res: Response)=>{
-    try{
-        const bookId = req.body.bookId;
-        const {id} = req.params;
+const clubBookEntryController = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.body.bookId;
+    const { id } = req.params;
 
-        const clubBook = await clubBookEntryService(id, bookId)
+    const clubBook = await clubBookEntryService(id, bookId);
 
-        res.status(200).json({
-            message: clubBook
-        })
-    }  catch (error: any) {
-        handleError(error, res);
-      }
-}
+    res.status(200).json(clubBook);
+  } catch (error: any) {
+    handleError(error, res);
+  }
+};
 
 export default clubBookEntryController;
